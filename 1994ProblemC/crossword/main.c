7942bfb446471db1146435b244eb567b9cbb7ee4
GautierCtre <57387842+GautierCtre@users.noreply.github.com>
2020-05-11 11:52:25 +0200

    fix compilation problem

--- 1994ProblemC/crossword/main.c
+++ 1994ProblemC/crossword/main.c
@@ -2,10 +2,10 @@
 
 int main() {
     int n, m, i, j, cases = 0;
-    char map[20][20];
+    char grid[20][20];
     while(scanf("%d %d", &n, &m) == 2) {
         for(i = 0; i < n; i++) {
-            scanf("%s", map[i]);
+            scanf("%s", grid[i]);
         }
         if(cases)
             puts("");
@@ -14,8 +14,8 @@ int main() {
         int num[20][20] = {}, cnt = 0;
         for(i = 0; i < n; i++) {
             for(j = 0; j < m; j++) {
-                if(i-1 < 0 || map[i-1][j] == '*' || j-1 < 0 || map[i][j-1] == '*') {
-                    if(map[i][j] != '*')
+                if(i-1 < 0 || grid[i-1][j] == '*' || j-1 < 0 || grid[i][j-1] == '*') {
+                    if(grid[i][j] != '*')
                         num[i][j] = ++cnt;
                 }
             }
@@ -24,10 +24,10 @@ int main() {
         for(i = 0; i < n; i++) {
             int g = 0;
             for(j = 0; j < m; j++) {
-                if(map[i][j] != '*') {
+                if(grid[i][j] != '*') {
                     if(g == 0)
                         printf("%3d.", num[i][j]);
-                    putchar(map[i][j]);
+                    putchar(grid[i][j]);
                     g = 1;
                 } else {
                     if(g) {
@@ -41,11 +41,11 @@ int main() {
         puts("Down");
         for(i = 0; i < n; i++) {
             for(j = 0; j < m; j++) {
-                if((i-1 < 0 || map[i-1][j] == '*') && map[i][j] != '*') {
+                if((i-1 < 0 || grid[i-1][j] == '*') && grid[i][j] != '*') {
                     int ti = i;
                     printf("%3d.", num[i][j]);
-                    while(ti < n && map[ti][j] != '*')
-                        putchar(map[ti++][j]);
+                    while(ti < n && grid[ti][j] != '*')
+                        putchar(grid[ti++][j]);
                     puts("");
                 }
             }
